fb453bbba12c54aee263bdf6cbeaa816d2a2230d
Nakazawa <luckystriq@gmail.com>
2025-06-22 21:15:02 +0900

    1

--- spike/slot_prod.py
+++ spike/slot_prod.py
@@ -11,7 +11,7 @@ gc.collect()
 MAX_IDLE_TIME = 120000  # Maximum idle time, unit: millisecond
 MAX_RUN_TIME = 600  # Maximum running time, unit: second
 
-# Command ID list
+# Command ID list                                                                               
 COMMAND_SET_MOTOR_FORWARD_POWER_ID = 201
 COMMAND_SET_MOTOR_BACKWARD_POWER_ID = 202
 COMMAND_SET_MOTOR_RELATIVE_POSITION_ID = 203
@@ -145,18 +145,17 @@ class LegoSpike(object):
         """モーターA/B・カラー・超音波センサーのみ取得し、他はコメントアウト"""
         try:
             # self.motor_left, self.motor_rightは__init__で取得済みインスタンスを必ず利用
-            motor_left = self.motor_left
-            motor_right = self.motor_right
             # モーター情報取得
-            motor_left_speed = motor_left.speed() if hasattr(motor_left, 'speed') else 0
-            motor_left_position = motor_left.absolute_position() if hasattr(motor_left, 'absolute_position') else 0
-            motor_left_relative = motor_left.relative_position() if hasattr(motor_left, 'relative_position') else 0
-            motor_left_power = motor_left.power() if hasattr(motor_left, 'power') else 0
-            
-            motor_right_speed = motor_right.speed() if hasattr(motor_right, 'speed') else 0
-            motor_right_position = motor_right.absolute_position() if hasattr(motor_right, 'absolute_position') else 0
-            motor_right_relative = motor_right.relative_position() if hasattr(motor_right, 'relative_position') else 0
-            motor_right_power = motor_right.power() if hasattr(motor_right, 'power') else 0
+            # motor_left = self.motor_left
+            # motor_right = self.motor_right
+            # motor_left_speed = motor_left.speed() if hasattr(motor_left, 'speed') else 0
+            # motor_left_position = motor_left.absolute_position() if hasattr(motor_left, 'absolute_position') else 0
+            # motor_left_relative = motor_left.relative_position() if hasattr(motor_left, 'relative_position') else 0
+            # motor_left_power = motor_left.power() if hasattr(motor_left, 'power') else 0
+            # motor_right_speed = motor_right.speed() if hasattr(motor_right, 'speed') else 0
+            # motor_right_position = motor_right.absolute_position() if hasattr(motor_right, 'absolute_position') else 0
+            # motor_right_relative = motor_right.relative_position() if hasattr(motor_right, 'relative_position') else 0
+            # motor_right_power = motor_right.power() if hasattr(motor_right, 'power') else 0
             
             # motor_arm, force_sensor, accel, gyro, 位置情報はコメントアウト
             # motor_arm_speed = self.motor_arm.speed() if hasattr(self.motor_arm, 'speed') else 0
@@ -176,10 +175,8 @@ class LegoSpike(object):
             sensor_data = {
                 "m": 0,  # message_type: 0 = sensor data
                 "p": [
-                    # モーターA (左) - Port 48
-                    [48, [motor_left_speed, motor_left_relative, motor_left_position, motor_left_power]],
-                    # モーターB (右) - Port 48  
-                    [48, [motor_right_speed, motor_right_relative, motor_right_position, motor_right_power]],
+                    # [48, [motor_left_speed, motor_left_relative, motor_left_position, motor_left_power]],  # モーターA (左) - Port 48
+                    # [48, [motor_right_speed, motor_right_relative, motor_right_position, motor_right_power]],  # モーターB (右) - Port 48
                     # # モーターC (アーム) - Port 49
                     # [49, [motor_arm_speed, motor_arm_relative, motor_arm_position, motor_arm_power]],
                     # # フォースセンサー - Port 63
@@ -210,8 +207,8 @@ class LegoSpike(object):
             return {
                 "m": 0,
                 "p": [
-                    [48, [0, 0, 0, 0]],  # モーターA
-                    [48, [0, 0, 0, 0]],  # モーターB
+                    # [48, [0, 0, 0, 0]],  # モーターA
+                    # [48, [0, 0, 0, 0]],  # モーターB
                     # [49, [0, 0, 0, 0]],  # モーターC
                     # [63, [0, 0, 0]],     # フォースセンサー
                     [61, [0, 0, 0, 0, 0]], # カラーセンサー
