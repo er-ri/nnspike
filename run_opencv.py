ae924525868a9427aaf5478be5b3633bd2e455a1
Nakazawa <luckystriq@gmail.com>
2025-06-22 22:45:15 +0900

    1

--- run_opencv.py
+++ run_opencv.py
@@ -249,14 +249,8 @@ def main(record_sensor_data=False, save_camera_video=False):
 
             # Send camera capture for remote monitoring
             try:
-                # 変更前: PNG形式でエンコード
-                # ret, buffer = cv2.imencode(".png", gray)
-                # img_encoded = buffer.tobytes()
-                # data = pickle.dumps(img_encoded)
-                # client_socket.sendall(struct.pack("L", len(data)) + data)
-
-                # 変更後: JPG形式（品質90）でエンコード
-                ret, buffer = cv2.imencode(".jpg", gray, [int(cv2.IMWRITE_JPEG_QUALITY), 90])
+                # 変更後: JPG形式（品質80）でエンコード
+                ret, buffer = cv2.imencode(".jpg", gray, [int(cv2.IMWRITE_JPEG_QUALITY), 80])
                 img_encoded = buffer.tobytes()
                 data = pickle.dumps(img_encoded)
                 client_socket.sendall(struct.pack("L", len(data)) + data)
