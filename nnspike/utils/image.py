250fafdaa2a6c869afa46a37d6bc578c6b406194
Nakazawa <luckystriq@gmail.com>
2025-06-25 00:56:06 +0900

    1

--- nnspike/utils/image.py
+++ nnspike/utils/image.py
@@ -77,7 +77,7 @@ def draw_driving_info(
         image = cv2.putText(
             image,
             f"{key} : {text}",
-            (50, 270 + index * 20),
+            (50, 230 + index * 20),  # 270→230で2行分(40px)上に移動
             cv2.FONT_HERSHEY_PLAIN,
             1,
             (255, 255, 255),
