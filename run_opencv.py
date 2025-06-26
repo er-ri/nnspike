c2faac491bf6b01a076b4fcfa88c21ba3bbc5aff
Nakazawa <luckystriq@gmail.com>
2025-06-27 02:37:52 +0900

    1

--- run_opencv.py
+++ run_opencv.py
@@ -547,9 +547,9 @@ def main(record_sensor_data=False, save_camera_video=False):
                 time.sleep(0.03 - elapsed)
     except KeyboardInterrupt:
         print("Interrupted by user")
-        action.brake_for_duration(duration=3.0)
+        action.brake_and_stop(duration=3.0)
     except Exception as e:
-        action.brake_for_duration(duration=3.0)
+        action.brake_and_stop(duration=3.0)
     finally:
         action.et.stop()
         cap.release()
