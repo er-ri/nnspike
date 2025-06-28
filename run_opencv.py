4a50ab30984fa5a67a24c365453d9f2c1cdacf26
Nakazawa <luckystriq@gmail.com>
2025-06-28 23:09:36 +0900

    1

--- run_opencv.py
+++ run_opencv.py
@@ -338,7 +338,7 @@ class ActionManager:
                 self.action_sent = True
                 self.turn_duration = 45 * 1.0 / 90
                 self.left_power = 0
-                self.right_power = 30
+                self.right_power = 40
             else:
                 if now - self.start_time >= self.turn_duration:
                     self.et.brake()
