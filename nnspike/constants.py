f1af7f696edd86547bf846ac4d4ae1d8b799d5d4
Nakazawa <luckystriq@gmail.com>
2025-06-29 21:18:59 +0900

    1

--- nnspike/constants.py
+++ nnspike/constants.py
@@ -1,8 +1,8 @@
 # Region of Interest for CNN model
-ROI_CNN = (50, 50, 590, 350)
+# ROI_CNN = (50, 50, 590, 350)
 
 # Region of Interest for opencv
-ROI_OPENCV = (180, 300, 460, 400)  # 左右をそれぞれ30pxずつ内側に狭めた例
+# ROI_OPENCV = (180, 300, 460, 400)  # 左右をそれぞれ30pxずつ内側に狭めた例
 
 # Camera and robot geometry constants
 CAMERA_HEIGHT = 0.20  # Camera height above ground in meters
@@ -10,5 +10,5 @@ CAMERA_FOCAL_LENGTH_PIXELS = 640  # Approximate focal length in pixels
 WHEELBASE = 0.10  # Distance between wheels in meters
 
 # OpenCV用のROIと画像サイズ（run_opencv.pyと同じ値）
-IMAGE_WIDTH = 640                  # カメラ画像の幅
-IMAGE_HEIGHT = 480                 # カメラ画像の高さ
+# IMAGE_WIDTH = 640                  # カメラ画像の幅
+# IMAGE_HEIGHT = 480                 # カメラ画像の高さ
