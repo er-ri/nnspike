07d69e93efe8e88e92e5e421c9e989576a8c5b3e
Nakazawa <luckystriq@gmail.com>
2025-06-30 21:22:15 +0900

    1

--- run_opencv.py
+++ run_opencv.py
@@ -33,8 +33,8 @@ CURVE_POWER = 30                   # 一律30
 CURVE_THRESHOLD_DEG = 10           # カーブ判定閾値
 STRAIGHT_THRESHOLD_DEG = 3         # 直線判定のしきい値
 SENSITIVITY = 0.7                  # 0.7（感度アップ）
-MAX_POWER_DIFF = 15                # 15（パワー差アップ）
-MAX_THETA_DEG = 25                 # 25（最大旋回角アップ）
+MAX_POWER_DIFF = 20                # 15（パワー差アップ）
+MAX_THETA_DEG = 30                 # 25（最大旋回角アップ）
 # 黒判定の閾値（反射光R: 40以下, color: 150以下なら黒と判定）
 BLACK_REFLECTED_THRESHOLD = 40
 BLACK_COLOR_THRESHOLD = 150
