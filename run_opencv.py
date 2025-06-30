c62f81c38071aed689dfb5cd0e66a287f2e9dc55
Nakazawa <luckystriq@gmail.com>
2025-06-30 22:59:18 +0900

    1

--- run_opencv.py
+++ run_opencv.py
@@ -119,7 +119,7 @@ class ActionManager:
         self.pid = PIDController(
             Kp=0.8,   # 0.5→0.7（応答性アップ）
             Ki=0,
-            Kd=0.05,  # 0.02→0.04（減衰強化）
+            Kd=0.04,  # 0.02→0.04（減衰強化）
             setpoint=0,
             output_limits=(-0.25, 0.25),
         )
