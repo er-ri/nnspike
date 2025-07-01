59325f2c87e2f5dc1b9fc5d6af1fc0f132c52fbc
Nakazawa <luckystriq@gmail.com>
2025-07-01 18:49:50 +0900

    1

--- nnspike/utils/control.py
+++ nnspike/utils/control.py
@@ -16,7 +16,7 @@ import json
 
 # ====【現場でよく調整する推奨パラメータ】====
 BASE_POWER = 30             # 通常走行時の基準パワー
-STRAIGHT_POWER = 50         # 直線判定時のパワー
+STRAIGHT_POWER = 30         # 直線判定時のパワー
 CURVE_POWER = 30            # カーブ判定時のパワー
 STRAIGHT_THRESHOLD_DEG = 3  # 直線とみなす角度しきい値[deg]
 CURVE_THRESHOLD_DEG = 10    # カーブとみなす角度しきい値[deg]
