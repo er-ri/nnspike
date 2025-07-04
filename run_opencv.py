fa4b5d73307a3b6820ced55e7d406764b3b45099
Nakazawa <luckystriq@gmail.com>
2025-07-04 21:47:50 +0900

    1

--- run_opencv.py
+++ run_opencv.py
@@ -57,9 +57,9 @@ from nnspike.utils import (
 )
 
 # ==== ユーザー調整用パラメータ（ここだけ編集すればOK） ====
-# ROI_OPENCV: OpenCV画像処理で使用する領域（左上x, 左上y, 右下x, 右下y）
-ROI_OPENCV = (20, 50, 620, 400)
+ # ROI_OPENCV: OpenCV画像処理で使用する領域（左上x, 左上y, 右下x, 右下y）
 ROI_BOTTLE = (100, 20, 540, 400)   # (x1, y1, x2, y2)
+ROI_OPENCV = (100, 50, 540, 400)   # ROI_BOTTLEと同じ横幅に変更
 IMAGE_WIDTH = 640                  # カメラ画像の幅
 IMAGE_HEIGHT = 480                 # カメラ画像の高さ
 # 黒判定の閾値（反射光R: 40以下, color: 150以下なら黒と判定）
