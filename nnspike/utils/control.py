36847cd84a3973e7731c61998b4ed1677525847a
Nakazawa <luckystriq@gmail.com>
2025-07-05 12:50:54 +0900

    1

--- nnspike/utils/control.py
+++ nnspike/utils/control.py
@@ -29,7 +29,7 @@ CAMERA_FOCAL_LENGTH_PIXELS = 640  # ピクセル単位のカメラ焦点距離�
 WHEELBASE = 0.10  # Distance between wheels in meters
 
 MAX_THETA_DEG = 20          # θの最大値[deg]（大きくすると直進安定性UP, ふらつき抑制）
-MAX_POWER_DIFF = 30         # PID補正による最大パワー差分（小さくするとふらつき抑制、カーブ追従性はやや低下）
+MAX_POWER_DIFF = 35         # PID補正による最大パワー差分（小さくするとふらつき抑制、カーブ追従性はやや低下）
 
 # ---【ライン検出Y座標（カメラ画像基準, camera.pyのOFFSET_Yと揃える）】---
 # ライントレース時に進行方向の基準とする画像内Y座標。
