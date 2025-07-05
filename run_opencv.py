caa132c5c2f0bb99436e9c0989559cbb7ea60f26
Nakazawa <luckystriq@gmail.com>
2025-07-05 15:40:02 +0900

    1

--- run_opencv.py
+++ run_opencv.py
@@ -438,9 +438,9 @@ class ActionManager:
             if not self.action_sent:
                 self.start_time = now
                 self.action_sent = True
-                self.arc_end_time = now + 0.5
+                self.arc_end_time = now + 1
                 self.left_power = 20  # 左モーターも動かす（弱く）
-                self.right_power = 50 # 右モーターを強く（左向き）
+                self.right_power = 60 # 右モーターを強く（左向き）
             else:
                 if now >= self.arc_end_time:
                     self.et.brake()
@@ -452,8 +452,8 @@ class ActionManager:
                 self.start_time = now
                 self.action_sent = True
                 self.arc_end_time = now + 2.0
-                self.left_power = 50  # 左モーターを強く
-                self.right_power = 20 # 右モーターを弱く（左迂回）
+                self.left_power = 60  # 左モーターを強く
+                self.right_power = 30 # 右モーターを弱く（左迂回）
             else:
                 if now >= self.arc_end_time:
                     self.et.brake()
