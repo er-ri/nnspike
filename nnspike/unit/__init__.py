ab17a7fa906a76adbd48d8ba2d5f2a2daa337e08
Nakazawa <luckystriq@gmail.com>
2025-07-24 23:00:13 +0900

    1

--- nnspike/unit/__init__.py
+++ nnspike/unit/__init__.py
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-from .actions import avoid_obstacle  # 1フレームstep型の障害物回避
-from .etrobot import ETRobot
-
-__all__ = ["avoid_obstacle", "ETRobot"]
-=======
 from .action_chain import ActionChain
 from .etrobot import ETRobot
 
 __all__ = ["ActionChain", "ETRobot"]
->>>>>>> wip/teamwork-li
