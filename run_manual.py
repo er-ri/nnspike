47d710beaab1e6f7c895ec87f22093fbb9094db6
Nakazawa <luckystriq@gmail.com>
2025-07-29 18:18:59 +0900

    1

--- run_manual.py
+++ run_manual.py
@@ -311,7 +311,6 @@ def main(record_sensor_data=False, save_camera_video=False, send_video_stream=Fa
                             et_right_position = status.motors[1].relative_position
                         except Exception:
                             et_right_position = None
-                    print(f"[DEBUG] et_right_position={et_right_position}")
                     _, right_x, _ = get_line_edges_at_y(frame, ROI_CNN, OFFSET_Y, 80)
                     if yellow_pixel_count > 14000 and yellow_cx is not None and et_right_position is not None and abs(et_right_position) <= 7000:
                         mode = Mode.AVOID_OBSTACLE
