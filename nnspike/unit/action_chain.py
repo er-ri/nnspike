f4d3fd30e4653158cbfff3b84222ffb5b17af8c4
Nakazawa <luckystriq@gmail.com>
2025-07-29 21:42:01 +0900

    1

--- nnspike/unit/action_chain.py
+++ nnspike/unit/action_chain.py
@@ -235,7 +235,7 @@ class ActionChain(object):
 
         # 1. 左旋回（1.5秒, 左:0, 右:30）
         if state["phase"] == 1:
-            if now - state["phase_start_time"] < 2.8:
+            if now - state["phase_start_time"] < 2.5:
                 left_speed, right_speed = 0, 30
                 return None, (left_speed, right_speed), Mode.BACK_AND_TURN1
             else:
