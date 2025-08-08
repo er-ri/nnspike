668b1a70eaf81810fb1d8948b005f02d0da9554b
Nakazawa <luckystriq@gmail.com>
2025-08-08 15:20:27 +0900

    1

--- run_manual.py
+++ run_manual.py
@@ -198,9 +198,9 @@ def main(record_sensor_data=False, save_camera_video=False, send_video_stream=Fa
         ),  # Direct radian limits for steering correction
     )
 
-    #et.move_arm(1, 1.0)  # アームを上げる（1: up）
-    #et.move_arm(0, 1.0)  # アームを下げる（0: down）
-    #et.move_arm(2, 0.5)  # アームを止める
+    et.move_arm(1, 1.0)  # アームを上げる（1: up）
+    et.move_arm(0, 1.0)  # アームを下げる（0: down）
+    et.move_arm(2, 0.5)  # アームを止める
     et.set_motor_relative_position(left_positon=0, right_position=0)
 
     try:
