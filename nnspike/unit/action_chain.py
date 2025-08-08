abd68c94e00aab6ec401316f3d3af9aaa7408edf
Nakazawa <luckystriq@gmail.com>
2025-08-08 17:41:51 +0900

    1

--- nnspike/unit/action_chain.py
+++ nnspike/unit/action_chain.py
@@ -745,7 +745,7 @@ class ActionChain(object):
         })
         status = self.et.get_spike_status()
 
-        # 0. 右エッジトレース→赤3000超でphase1へ
+        # 0. 右エッジトレース→赤2000超でphase1へ
         if state["phase"] == 0:
             _, right_x, _ = get_line_edges_at_y(image=image, roi=ROI_CNN, target_y=OFFSET_Y, threshold_value=80)
             target_x = right_x if right_x is not None else (self.x1 + self.x2) // 2
