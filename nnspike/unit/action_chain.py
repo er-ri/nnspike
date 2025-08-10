c594f701230b9faf676b7cc19c128690748c26f3
Nakazawa <luckystriq@gmail.com>
2025-08-10 15:37:49 +0900

    1

--- nnspike/unit/action_chain.py
+++ nnspike/unit/action_chain.py
@@ -751,12 +751,12 @@ class ActionChain(object):
                 # ステータス取得失敗時は継続
                 return None, (40, 70), Mode.AVOID_OBSTACLE
 
-        # 1. 右旋回（右モーター650ユニット移動まで, 左:70, 右:50）
+        # 1. 右旋回（右モーター650ユニット移動まで, 左:70, 右:40）
         if state["phase"] == 1:
             if status is not None and status.motors.get("B") is not None and state["right_position_start"] is not None:
                 current_pos = abs(status.motors["B"].relative_position)
                 if abs(current_pos - state["right_position_start"]) < 650:
-                    return None, (70, 50), Mode.AVOID_OBSTACLE
+                    return None, (80, 50), Mode.AVOID_OBSTACLE
                 else:
                     # 550ユニット到達したので次のフェーズへ
                     state["phase"] = 2
