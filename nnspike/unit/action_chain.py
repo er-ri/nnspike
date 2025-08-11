0b75ff5dce20bab40fdb5b9faa46d3236073b034
Nakazawa <luckystriq@gmail.com>
2025-08-11 18:10:17 +0900

    １

--- nnspike/unit/action_chain.py
+++ nnspike/unit/action_chain.py
@@ -376,7 +376,7 @@ class ActionChain(object):
                     state["right_position_start"] = None
             if status is not None and status.motors.get("B") is not None and state["right_position_start"] is not None:
                 current_pos = abs(status.motors["B"].relative_position)
-                if abs(current_pos - state["right_position_start"]) < 400:
+                if abs(current_pos - state["right_position_start"]) < 390:
                     return None, (0, 30), Mode.CARRY_BOTTLE1
             # 400超えたら次フェーズへ
             state["phase"] = 4
