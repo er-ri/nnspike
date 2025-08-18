7bd51c650f5cfe49b04e8d82b2b2d944190fcec1
Nakazawa <luckystriq@gmail.com>
2025-08-18 14:41:14 +0900

    1

--- nnspike/unit/action_chain.py
+++ nnspike/unit/action_chain.py
@@ -697,7 +697,7 @@ class ActionChain(object):
             position_start = phase.get_position_start("position_start")
             current_pos = self.get_motor_position(self.course, status=status)
             min_limit = 500
-            max_limit = 1000
+            max_limit = 1500
             position_delta = abs(current_pos - position_start)
             position_limit_reached = position_delta >= max_limit
 
