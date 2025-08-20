ec53dc12e0f56abae381ed5614b61101f13ff3bf
Nakazawa <luckystriq@gmail.com>
2025-08-20 17:30:10 +0900

    1

--- run_manual.py
+++ run_manual.py
@@ -520,24 +520,29 @@ def main(record_sensor_data=False, save_camera_video=False, send_video_stream=Fa
                             print(f"Left position {abs(left_pos)} > 22000, switching to CARRY_BOTTLE1")
                         else:
                             # courseによって左右判定を反転
-                            # abs(left_pos)が16000～19000のときは強制的にleft_xをtarget_xにする
+                            # abs(left_pos)が15000～19000のときは強制的にleft_xをtarget_xにする
                             if left_pos is not None and 15000 <= abs(left_pos) < 19000:
                                 left_x, _, _ = get_line_edges_at_y(frame, ROI_CNN, OFFSET_Y, 80)
                                 target_x = left_x if left_x is not None else (x1 + x2) // 2
+                                print(f"[DEBUG] left_pos={left_pos}, trace=left")
                             # abs(left_pos)が19000～21000のときは強制的にright_xをtarget_xにする
                             elif left_pos is not None and 19000 <= abs(left_pos) <= 21000:
                                 _, right_x, _ = get_line_edges_at_y(frame, ROI_CNN, OFFSET_Y, 80)
                                 target_x = right_x if right_x is not None else (x1 + x2) // 2
+                                print(f"[DEBUG] left_pos={left_pos}, trace=right")
                             # abs(left_pos)が21000より大きい場合は左
                             elif left_pos is not None and abs(left_pos) > 21000:
                                 left_x, _, _ = get_line_edges_at_y(frame, ROI_CNN, OFFSET_Y, 80)
                                 target_x = left_x if left_x is not None else (x1 + x2) // 2
+                                print(f"[DEBUG] left_pos={left_pos}, trace=left")
                             elif nvidia_mode_prediction == Mode.FOLLOW_LEFT_EDGE.value:  # 左エッジ
                                 _, right_x, _ = get_line_edges_at_y(frame, ROI_CNN, OFFSET_Y, 80)
                                 target_x = right_x if right_x is not None else (x1 + x2) // 2
+                                print(f"[DEBUG] left_pos={left_pos}, trace=right")
                             else:  # 左モード以外はすべて右エッジ
                                 left_x, _, _ = get_line_edges_at_y(frame, ROI_CNN, OFFSET_Y, 80)
                                 target_x = left_x if left_x is not None else (x1 + x2) // 2
+                                print(f"[DEBUG] left_pos={left_pos}, trace=left")
                     else:
                         if right_pos is not None and abs(right_pos) > 22000:
                             mode = Mode.CARRY_BOTTLE1
