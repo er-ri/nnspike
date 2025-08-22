40f35696ebc8fd03251c19d44716b3cddd431282
Nakazawa <luckystriq@gmail.com>
2025-08-22 15:54:23 +0900

    1

--- run_manual.py
+++ run_manual.py
@@ -586,7 +586,7 @@ def main(record_sensor_data=False, save_camera_video=False, send_video_stream=Fa
                 if 'pos_check' in locals() and pos_check is not None and abs(pos_check) < 22000:
                     current_base_speed = 55
                 else:
-                    current_base_speed = 45
+                    current_base_speed =　BASE_SPEED
 
                 steering_correction = pid.update(theta)
 
