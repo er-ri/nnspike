fe63e84c4722d5ac67210072a194d05f8010daf5
Nakazawa <luckystriq@gmail.com>
2025-08-22 16:43:58 +0900

    1

--- nnspike/unit/action_chain.py
+++ nnspike/unit/action_chain.py
@@ -368,9 +368,9 @@ class ActionChain(object):
             position_diff = abs(current_pos - position_start)
             if position_diff < 100:
                 if self.course == "right":
-                    return None, (60, 30), Mode.AVOID_OBSTACLE
+                    return None, (60, 35), Mode.AVOID_OBSTACLE
                 else:
-                    return None, (30, 60), Mode.AVOID_OBSTACLE
+                    return None, (35, 60), Mode.AVOID_OBSTACLE
             else:
                 phase.next_phase()
                 phase.set_position_start("position_start", self.get_motor_position(self.course, status=status))
