7540fa5685709985fd3e16ff210beca9b6417e8a
Nakazawa <luckystriq@gmail.com>
2025-08-26 14:50:44 +0900

    １

--- run_manual.py
+++ run_manual.py
@@ -466,6 +466,7 @@ def main(record_sensor_data=False, save_camera_video=False, send_video_stream=Fa
                         current_base_speed = 100
                         theta_under_3_start_pos = None
                     else:
+                        print(f"[DEBUG] HIGH_SPEED_AVOID: theta={theta}")
                         if theta < 3:
                             if theta_under_3_start_pos is None:
                                 theta_under_3_start_pos = edge_pos
