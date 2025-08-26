c388ed337fb39df4dd579cc64b871cd7e3e2cca2
Nakazawa <luckystriq@gmail.com>
2025-08-26 14:42:50 +0900

    1

--- nnspike/unit/action_chain.py
+++ nnspike/unit/action_chain.py
@@ -416,7 +416,7 @@ class ActionChain(object):
         if phase.get_phase() == 1:
             print("[DEBUG] phase=1 (yellow area check)")
             yellow_cx, _, yellow_pixel_count = find_bottle_center(image=image, color="yellow")
-            if yellow_pixel_count > 18000:
+            if yellow_pixel_count > 14000:
                 phase.next_phase()
                 phase.set_position_start("position_start", self.get_motor_position(self.course, status=status))
             else:
