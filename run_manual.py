5d6a20436fd73955de295548bbda7634395ea7d6
Nakazawa <luckystriq@gmail.com>
2025-08-26 14:30:18 +0900

    １

--- run_manual.py
+++ run_manual.py
@@ -460,10 +460,12 @@ def main(record_sensor_data=False, save_camera_video=False, send_video_stream=Fa
                     # courseに応じてposを切り替え（int型のみ）
                     # edge_pos: courseに応じてint/float/tuple/list/Noneを直接受ける
                     edge_pos = left_pos if course == "left" else right_pos
+                    edge_pos = 0 if edge_pos is None else (edge_pos[0] if type(edge_pos) in (tuple, list) else edge_pos)
+                    theta = 0 if theta is None else (theta[0] if type(theta) in (tuple, list) else theta)
                     if edge_pos < 5000:
                         current_base_speed = 100
                         theta_under_3_start_pos = None
-                    else:
+                    else:ｑｑｑ
                         if theta < 3:
                             if theta_under_3_start_pos is None:
                                 theta_under_3_start_pos = edge_pos
