3440fe0c18dfeb077566ca30f10466e0ae832ef4
Nakazawa <luckystriq@gmail.com>
2025-08-26 13:26:57 +0900

    1

--- run_manual.py
+++ run_manual.py
@@ -456,8 +456,13 @@ def main(record_sensor_data=False, save_camera_video=False, send_video_stream=Fa
                     current_base_speed = 100
                 elif mode == Mode.HIGH_SPEED_AVOID:
                     # courseに応じてposを切り替え
-                    edge_pos = left_pos if course == "left" else right_pos
-                    if edge_pos is not None and edge_pos < 5000:
+                    # edge_posがtuple型の場合はintに変換
+                    raw_pos = left_pos if course == "left" else right_pos
+                    if isinstance(raw_pos, tuple):
+                        edge_pos = raw_pos[0]
+                    else:
+                        edge_pos = raw_pos
+                    if edge_pos is not None and isinstance(edge_pos, (int, float)) and edge_pos < 5000:
                         current_base_speed = 100
                         theta_under_3_start_pos = None
                     else:
