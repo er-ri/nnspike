5052ee9a61c4ac459f97f875240936d5cac35ca3
Nakazawa <luckystriq@gmail.com>
2025-08-26 13:44:04 +0900

    1

--- nnspike/unit/action_chain.py
+++ nnspike/unit/action_chain.py
@@ -412,15 +412,7 @@ class ActionChain(object):
         phase = self._phase
         status = self._status
 
-        if phase.get_phase() == 0:
-            target_x = self.get_target_x_by_course(image, offset_y=ROI_CNN, course=self.course)
-            yellow_cx, _, yellow_pixel_count = find_bottle_center(image, "yellow")
-            if yellow_pixel_count > 18000 and yellow_cx is not None:
-                phase.next_phase()
-                phase.set_position_start("position_start", self.get_motor_position(self.course, status=status))
-            elif yellow_pixel_count > 3000 and yellow_cx is not None:
-                target_x = yellow_cx[0]
-            return target_x, None, Mode.HIGH_SPEED_AVOID
+        return 160, None, Mode.HIGH_SPEED_AVOID
 
         print("[avoid_obstacle] Unexpected state reached.")
         return None, None, Mode.HIGH_SPEED_AVOID
