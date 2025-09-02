aa0a0b8960028debeadb503ec67f018603a07757
Nakazawa <luckystriq@gmail.com>
2025-09-02 15:41:28 +0900

    1

--- nnspike/unit/action_chain.py
+++ nnspike/unit/action_chain.py
@@ -435,7 +435,7 @@ class ActionChain(object):
             current_pos = self.get_motor_position(self.course, status=status)
             position_diff = abs(current_pos - position_start)
             print(f"[DEBUG] phase=2 position_diff={position_diff} current_pos={current_pos}")
-            if position_diff < 250:
+            if position_diff < 300:
                 if self.course == "right":
                     return None, (40, 70, 0), Mode.HIGH_SPEED_AVOID
                 else:
@@ -450,7 +450,7 @@ class ActionChain(object):
             current_pos = self.get_motor_position(self.course, status=status)
             position_diff = abs(current_pos - position_start)
             print(f"[DEBUG] phase=3 position_diff={position_diff} current_pos={current_pos}")
-            if position_diff < 150:
+            if position_diff < 200:
                 return None, (BASE_SPEED, BASE_SPEED, 0), Mode.HIGH_SPEED_AVOID
             else:
                 phase.next_phase()
