3c5a73be84ffab6b777f94e9b6c05bfa8b9171c0
Nakazawa <luckystriq@gmail.com>
2025-09-09 21:35:37 +0900

    1

--- nnspike/unit/action_chain.py
+++ nnspike/unit/action_chain.py
@@ -545,7 +545,7 @@ class ActionChain(object):
                     return None, (30, 60, 0), Mode.HIGH_SPEED_AVOID
                 else:
                     return None, (60, 30, 0), Mode.HIGH_SPEED_AVOID
-            elif distance < 200:
+            elif distance < 500:
                 if is_vertical_black_line_detected(image, roi=ROI_LOOP, center_tolerance=120):
                     print(f"[DEBUG] phase6→phase7: distance={distance} current_pos={current_pos} (vertical black line detected)")
                     phase.next_phase()
@@ -579,7 +579,7 @@ class ActionChain(object):
                 phase.next_phase()
                 phase.set_position_start("position_start", self.get_motor_position(self.course, status=status))
             else:
-                target_x = self.get_target_x_by_course(image, OFFSET_Y, self.opposite_course)
+                target_x = self.get_target_x_by_course(image, OFFSET_Y, self.course)
                 return target_x, (0, 0, BASE_SPEED), Mode.HIGH_SPEED_AVOID
 
         # phase8: 右モーターは必ず一定距離旋回し、その後垂直黒ライン判定で次フェーズへ
