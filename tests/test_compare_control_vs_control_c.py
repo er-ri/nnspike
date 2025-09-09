043b856830eb482f8b298a7790776be7b79a8dcd
Nakazawa <luckystriq@gmail.com>
2025-09-10 02:50:28 +0900

    １

--- tests/test_compare_control_vs_control_c.py
+++ tests/test_compare_control_vs_control_c.py
@@ -1,7 +1,6 @@
-# 共有ディレクトリのcv2を優先してimport（例: //192.168.137.40/msad/opencv）
+# 共有ディレクトリのcv2を優先してimport（ラズパイ内で/mnt/opencvにマウントしている場合）
 import sys
-sys.path.insert(0, r"//192.168.137.40/msad/opencv")  # 必要に応じてパスを修正
-# Windowsからアクセスする場合は r"\\192.168.137.40\msad\opencv" も可
+sys.path.insert(0, "/mnt/opencv")  # 必要に応じてパスを修正
 # import前にsys.pathを確認したい場合は下記を有効化
 # print("sys.path:", sys.path)
 
