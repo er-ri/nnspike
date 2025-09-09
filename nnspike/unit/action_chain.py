efb0e1a44985634db41f3fc4829755aadc7756a6
Nakazawa <luckystriq@gmail.com>
2025-09-09 21:34:07 +0900

    1

--- nnspike/unit/action_chain.py
+++ nnspike/unit/action_chain.py
@@ -545,7 +545,7 @@ class ActionChain(object):
                     return None, (30, 60, 0), Mode.HIGH_SPEED_AVOID
                 else:
                     return None, (60, 30, 0), Mode.HIGH_SPEED_AVOID
-            elif distance < 500:
+            elif distance < 200:
                 if is_vertical_black_line_detected(image, roi=ROI_LOOP, center_tolerance=120):
                     print(f"[DEBUG] phase6→phase7: distance={distance} current_pos={current_pos} (vertical black line detected)")
                     phase.next_phase()
