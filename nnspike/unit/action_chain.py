1af7855e96d35cc9c55c00cc4a55b20c3260e084
Nakazawa <luckystriq@gmail.com>
2025-09-10 19:46:39 +0900

    1

--- nnspike/unit/action_chain.py
+++ nnspike/unit/action_chain.py
@@ -545,9 +545,9 @@ class ActionChain(object):
             position_start = phase.get_position_start("position_start")
             current_pos = self.get_motor_position(self.course, status=status)
             position_diff = abs(current_pos - position_start)
-            # 上限1400を追加
-            if position_diff > 1400:
-                print(f"[DEBUG] phase10→phase11: position_diff={position_diff} current_pos={current_pos} >= 1400 (force next phase)")
+            # 上限1500を追加
+            if position_diff > 1500:
+                print(f"[DEBUG] phase10→phase11: position_diff={position_diff} current_pos={current_pos} >= 1500 (force next phase)")
                 phase.next_phase()
                 phase.set_position_start("position_start", self.get_motor_position(self.course, status=status))
             elif position_diff < 500:
