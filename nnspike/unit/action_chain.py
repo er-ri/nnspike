51824fba635fee69ed61e5bac4bf98667730343b
Nakazawa <luckystriq@gmail.com>
2025-09-10 14:26:11 +0900

    １

--- nnspike/unit/action_chain.py
+++ nnspike/unit/action_chain.py
@@ -597,8 +597,10 @@ class ActionChain(object):
                 phase.set_position_start("position_start", self.get_motor_position(self.course, status=status))
             elif position_diff < 500:
                 if self.course == "right":
+                    print(f"[DEBUG] phase8: return (49, 70, 0), Mode.HIGH_SPEED_AVOID (position_diff={position_diff}, current_pos={current_pos})")
                     return None, (49, 70, 0), Mode.HIGH_SPEED_AVOID
                 else:
+                    print(f"[DEBUG] phase8: return (70, 49, 0), Mode.HIGH_SPEED_AVOID (position_diff={position_diff}, current_pos={current_pos})")
                     return None, (70, 49, 0), Mode.HIGH_SPEED_AVOID
             # 一定距離進んだら、垂直黒ライン判定
             elif is_vertical_black_line_detected(image, roi=ROI_LOOP, center_tolerance=200):
@@ -611,8 +613,10 @@ class ActionChain(object):
                 phase.set_position_start("position_start", self.get_motor_position(self.course, status=status))
             else:
                 if self.course == "right":
+                    print(f"[DEBUG] phase8: return (49, 70, 0), Mode.HIGH_SPEED_AVOID (position_diff={position_diff}, current_pos={current_pos})")
                     return None, (49, 70, 0), Mode.HIGH_SPEED_AVOID
                 else:
+                    print(f"[DEBUG] phase8: return (70, 49, 0), Mode.HIGH_SPEED_AVOID (position_diff={position_diff}, current_pos={current_pos})")
                     return None, (70, 49, 0), Mode.HIGH_SPEED_AVOID
 
         # phase9: corner検出で状態リセット、他はHIGH_SPEED_AVOID継続
