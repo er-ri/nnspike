6f754db065a3da0cd56e2e3b25f67a0289414dd4
Nakazawa <luckystriq@gmail.com>
2025-09-10 17:39:09 +0900

    1

--- nnspike/unit/action_chain.py
+++ nnspike/unit/action_chain.py
@@ -552,10 +552,10 @@ class ActionChain(object):
                 phase.set_position_start("position_start", self.get_motor_position(self.course, status=status))
             elif position_diff < 500:
                 if self.course == "right":
-                    return None, (49, 70, 0), Mode.HIGH_SPEED_AVOID
+                    return None, (47, 70, 0), Mode.HIGH_SPEED_AVOID
                 else:
-                    return None, (70, 49, 0), Mode.HIGH_SPEED_AVOID
-            elif is_vertical_black_line_detected(image, roi=ROI_LOOP, center_tolerance=120):
+                    return None, (70, 47, 0), Mode.HIGH_SPEED_AVOID
+            elif is_vertical_black_line_detected(image, roi=ROI_LOOP, center_tolerance=150):
                 print(f"[DEBUG] phase10→phase11: position_diff={position_diff} current_pos={current_pos} >= 500 and vertical_black_line_detected")
                 self.pid.Kp = 50
                 self.pid.Ki = 0
@@ -565,9 +565,9 @@ class ActionChain(object):
                 phase.set_position_start("position_start", self.get_motor_position(self.course, status=status))
             else:
                 if self.course == "right":
-                    return None, (49, 70, 0), Mode.HIGH_SPEED_AVOID
+                    return None, (47, 70, 0), Mode.HIGH_SPEED_AVOID
                 else:
-                    return None, (70, 49, 0), Mode.HIGH_SPEED_AVOID
+                    return None, (70, 47, 0), Mode.HIGH_SPEED_AVOID
 
         # phase11: 青面積判定または右モーターが一定距離進んだらDOUBLE_LOOP、そうでなければHIGH_SPEED_AVOID継続
         if phase.get_phase() == 11:
