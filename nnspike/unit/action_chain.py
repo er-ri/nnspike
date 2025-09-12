3b68bc3bd56c462a736a274c2bdd16df227233bd
Nakazawa <luckystriq@gmail.com>
2025-09-12 17:23:14 +0900

    1

--- nnspike/unit/action_chain.py
+++ nnspike/unit/action_chain.py
@@ -378,7 +378,6 @@ class ActionChain(object):
         # phase0: 領域検出で次フェーズへ。未検出時は中央追従・回避モード返却
         if phase.get_phase() == 0:
             _, _, yellow_pixel_count = find_bottle_center(image=image, color="yellow", roi=ROI_COLOR)
-            center_line_detected = is_center_line_detected(image)
             if yellow_pixel_count > 5000:
                 print(f"[DEBUG] mode={Mode.AVOID_OBSTACLE.value} | phase={phase.get_phase()} | yellow_pixel_count={yellow_pixel_count} > 5000")
                 phase.next_phase()
