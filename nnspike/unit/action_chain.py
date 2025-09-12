046574f8b9ea07a03424e50586ca6f6ee546d802
Nakazawa <luckystriq@gmail.com>
2025-09-12 17:12:52 +0900

    1

--- nnspike/unit/action_chain.py
+++ nnspike/unit/action_chain.py
@@ -553,17 +553,17 @@ class ActionChain(object):
                 phase.set_position_start("position_start", self.get_motor_position(self.course, status=status))
             elif center_line_detected:
                 if color_type != "white":
-                    self.pid.Kp = 5
+                    self.pid.Kp = 12
                     self.pid.Ki = 0
                     self.pid.Kd = 0
-                    self.pid.output_limits = (-3, 3)
+                    self.pid.output_limits = (-2, 2)
                     target_x = self.get_target_x_by_course_safe(image, self.opposite_course)
                     return target_x, (0, 0, ULTRA_HIGH_SPEED), Mode.AVOID_OBSTACLE
                 else:
-                    self.pid.Kp = 10
+                    self.pid.Kp = 12
                     self.pid.Ki = 0
                     self.pid.Kd = 0
-                    self.pid.output_limits = (-5, 5)
+                    self.pid.output_limits = (-2, 2)
                     target_x = self.get_target_x_by_course_safe(image, self.opposite_course)
                     return target_x, (0, 0, HIGH_SPEED_BASE), Mode.AVOID_OBSTACLE
             else:
