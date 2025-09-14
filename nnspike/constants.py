71bab05159a88fd69ac6ad4ba87e4a4da8deb8a2
Nakazawa <luckystriq@gmail.com>
2025-09-14 13:50:16 +0900

    1

--- nnspike/constants.py
+++ nnspike/constants.py
@@ -22,7 +22,7 @@ OFFSET_Y = 470  # 0.20 meters to the ground
 
 # Base speed for robot movement (used throughout action logic)
 BASE_SPEED = 45
-HIGH_SPEED_BASE = 80
+HIGH_SPEED_BASE = 98
 
 # Behavior Mode
 class Mode(Enum):
