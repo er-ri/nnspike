a800c657a999df4f3b727f267109b1497ce6b90a
Nakazawa <luckystriq@gmail.com>
2025-09-17 02:58:03 +0900

    １

--- nnspike/unit/spike_status.py
+++ nnspike/unit/spike_status.py
@@ -201,6 +201,9 @@ class SpikeStatus:
             self._last_gyro_y = gyro.y
             self._last_gyro_z = gyro.z
 
+        # ジャイロ積分値を表示
+        print(f"[SpikeStatus] gyro_integrated: x={self._gyro_angle_x:.2f}, y={self._gyro_angle_y:.2f}, z={self._gyro_angle_z:.2f}")
+
         self.battery = BatteryStatus.from_dict(parsed_data.get("battery", {}))
 
     def get_gyro_angle_x(self) -> float:
