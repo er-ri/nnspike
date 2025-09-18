4cc8d8795e6976de9649958e21adcbdd857ca90f
Nakazawa <luckystriq@gmail.com>
2025-09-18 22:53:26 +0900

    １

--- nnspike/unit/spike_status.py
+++ nnspike/unit/spike_status.py
@@ -162,7 +162,7 @@ class SpikeStatus:
         if hasattr(self, '_last_motor_update_time') and self._last_motor_update_time is not None:
             dt = now - self._last_motor_update_time
             dt_ms = dt * 1000 if dt is not None else None
-            print(f"interval: {dt_ms:.2f} ms | type: {self.message_type}")
+            print(f"interval: {dt_ms:.2f} ms | type: {self.message_type} | raw: {self.raw_data}")
         self._last_motor_update_time = now
 
         # Update motors
