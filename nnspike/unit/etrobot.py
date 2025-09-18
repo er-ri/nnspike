2d212add09a437fbe8436927c9f6e1822f7d8cbe
Nakazawa <luckystriq@gmail.com>
2025-09-18 22:23:19 +0900

    1

--- nnspike/unit/etrobot.py
+++ nnspike/unit/etrobot.py
@@ -66,7 +66,7 @@ class ETRobot(object):
                 print(f"Error processing data: {e}")
                 print(f"Raw data: {received_data}")
 
-        time.sleep(0.001)
+        time.sleep(0.01)
 
     def __update_last_spike_status(self) -> None:
         """
