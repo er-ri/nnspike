18b6cabe35cb7cb7ab9c492022b72d4bf35d7d1e
Nakazawa <luckystriq@gmail.com>
2025-09-20 17:42:23 +0900

    １

--- run_realtime.py
+++ run_realtime.py
@@ -400,10 +400,10 @@ def main(record_sensor_data=False, save_camera_video=False, course="right", cour
                                 # オーバーシュート分だけ逆方向に動かす（error < 0なら右回転、error > 0なら左回転）
                                 if error < 0:
                                     # et.set_motor_backward_speed(left_speed=0, right_speed=20)  # 右回転（左モータ0、右のみ逆）
-                                    et.set_motor_speed(left_speed=20, right_speed=-20)  # 両方逆転（右回転）
+                                    et.set_motor_speed(left_speed=10, right_speed=-10)  # 両方逆転（右回転）
                                 else:
                                     # et.set_motor_forward_speed(left_speed=0, right_speed=20)  # 左回転（左モータ0、右のみ正）
-                                    et.set_motor_speed(left_speed=-20, right_speed=20)  # 両方正転（左回転）
+                                    et.set_motor_speed(left_speed=-10, right_speed=10)  # 両方正転（左回転）
                                 print(f"[TURN_LEFT][ADJUST] yaw={et.get_yaw():.2f}, ref_yaw={turn_left_reference_yaw:.2f}, error={error:.2f}, elapsed={elapsed:.2f}s")
                             else:
                                 et.set_motor_forward_speed(left_speed=0, right_speed=0)
@@ -466,10 +466,10 @@ def main(record_sensor_data=False, save_camera_video=False, course="right", cour
                                 # オーバーシュート分だけ逆方向に動かす（error < 0なら左回転、error > 0なら右回転）
                                 if error < 0:
                                     # et.set_motor_forward_speed(left_speed=20, right_speed=0)  # 左回転（右モータ0、左のみ正）
-                                    et.set_motor_speed(left_speed=20, right_speed=-20)  # 両方正転（左回転）
+                                    et.set_motor_speed(left_speed=10, right_speed=-10)  # 両方正転（左回転）
                                 else:
                                     # et.set_motor_backward_speed(left_speed=20, right_speed=0)  # 右回転（右モータ0、左のみ逆）
-                                    et.set_motor_speed(left_speed=-20, right_speed=20)  # 両方逆転（右回転）
+                                    et.set_motor_speed(left_speed=-10, right_speed=10)  # 両方逆転（右回転）
                                 print(f"[TURN_RIGHT][ADJUST] yaw={et.get_yaw():.2f}, ref_yaw={turn_right_reference_yaw:.2f}, error={error:.2f}, elapsed={elapsed:.2f}s")
                             else:
                                 et.set_motor_forward_speed(left_speed=0, right_speed=0)
