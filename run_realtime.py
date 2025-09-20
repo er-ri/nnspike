9ab24fc61f92f5eeb67b71d2c747ededb9c4138c
Nakazawa <luckystriq@gmail.com>
2025-09-20 18:31:16 +0900

    １

--- run_realtime.py
+++ run_realtime.py
@@ -509,12 +509,12 @@ def main(record_sensor_data=False, save_camera_video=False, course="right", cour
                     right_pos = 0
                 # 直進1: 右の走行距離が1000未満
                 if test_phase == "straight1":
-                    if right_pos < 1000:
+                    if test_phase == "straight1" and right_pos < 1000:
                         et.set_start_yaw(test_phase_start_yaw_init)  # 起動時のヨー
                         left_speed, right_speed = et.yaw_straight_control(base_speed=HIGH_SPEED_BASE)
                         et.set_motor_forward_speed(left_speed=int(left_speed), right_speed=int(right_speed))
                         print(f"[TEST] straight1 right_pos={right_pos}")
-                    else:
+                    elif test_phase == "straight1" and right_pos >= 1000:
                         test_phase = "turn_right"
                         test_phase_start_yaw = test_phase_start_yaw_init  # 起動時のヨーを絶対基準
                         print(f"[TEST] start turn_right phase yaw={test_phase_start_yaw:.2f}")
