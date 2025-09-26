1aa35614c7d6d1b7919a1cac9f113674f92cba61
Nakazawa <luckystriq@gmail.com>
2025-09-26 15:45:21 +0900

    1

--- nnspike/unit/fast_lap_chain.py
+++ nnspike/unit/fast_lap_chain.py
@@ -567,7 +567,7 @@ class FastLapChain(object):
             position_start = phase.get_position_start("position_start")
             current_pos = self.get_motor_position(self.course)
             position_diff = abs(current_pos - position_start)
-            if position_diff < 1500:
+            if position_diff < 1200:
                 left_speed, right_speed = et.yaw_straight_control(base_speed=HIGH_SPEED_BASE)
                 return None, (left_speed, right_speed, 0), Mode.SHORTCUT_LAP2
             else:
@@ -586,9 +586,9 @@ class FastLapChain(object):
                     self.et.set_start_yaw(start_yaw + 90.0)
             else:
                 if self.course == "right":
-                    return None, (80, 100, 0), Mode.SHORTCUT_LAP2
+                    return None, (70, 100, 0), Mode.SHORTCUT_LAP2
                 else:
-                    return None, (100, 80, 0), Mode.SHORTCUT_LAP2
+                    return None, (100, 70, 0), Mode.SHORTCUT_LAP2
 
         # フェーズ6: position_startとの差分1000未満ならyaw_straight_control直進。1000以上で次フェーズ
         if phase.get_phase() == 6:
@@ -614,9 +614,9 @@ class FastLapChain(object):
                     self.et.set_start_yaw(start_yaw + 180.0)
             else:
                 if self.course == "right":
-                    return None, (80, 100, 0), Mode.SHORTCUT_LAP2
+                    return None, (70, 100, 0), Mode.SHORTCUT_LAP2
                 else:
-                    return None, (100, 80, 0), Mode.SHORTCUT_LAP2
+                    return None, (100, 70, 0), Mode.SHORTCUT_LAP2
 
         # フェーズ8: position_startとの差分2000未満ならyaw_straight_control直進。2000以上で次フェーズ
         if phase.get_phase() == 8:
