a7650df74b80922af33aafa2f69027089d709f46
Nakazawa <luckystriq@gmail.com>
2025-09-26 20:29:59 +0900

    1

--- nnspike/unit/fast_lap_chain.py
+++ nnspike/unit/fast_lap_chain.py
@@ -569,7 +569,7 @@ class FastLapChain(object):
             current_pos = self.get_motor_position(self.course)
             position_diff = abs(current_pos - position_start)
             # if position_diff < 1750:
-            if position_diff < 1000:
+            if position_diff < 1100:
                 left_speed, right_speed = et.yaw_straight_control(base_speed=HIGH_SPEED_BASE)
                 return None, (left_speed, right_speed, 0), Mode.SHORTCUT_LAP2
             else:
@@ -597,7 +597,7 @@ class FastLapChain(object):
             position_start = phase.get_position_start("position_start")
             current_pos = self.get_motor_position(self.course)
             position_diff = abs(current_pos - position_start)
-            threshold = 2100 if self.course == "left" else 2000
+            threshold = 2200 if self.course == "left" else 2000
             if position_diff < threshold:
                 left_speed, right_speed = et.yaw_straight_control(base_speed=HIGH_SPEED_BASE)
                 return None, (left_speed, right_speed, 0), Mode.SHORTCUT_LAP2
