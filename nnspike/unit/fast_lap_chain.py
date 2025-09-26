18501fc6ab5506f67608afa60bc3809a140dcb5a
Nakazawa <luckystriq@gmail.com>
2025-09-26 20:37:01 +0900

    1

--- nnspike/unit/fast_lap_chain.py
+++ nnspike/unit/fast_lap_chain.py
@@ -543,7 +543,7 @@ class FastLapChain(object):
             position_start = phase.get_position_start("position_start")
             current_pos = self.get_motor_position(self.course)
             position_diff = abs(current_pos - position_start)
-            if position_diff < 200:
+            if position_diff < 100:
                 left_speed, right_speed = et.yaw_straight_control(base_speed=HIGH_SPEED_BASE)
                 return None, (left_speed, right_speed, 0), Mode.SHORTCUT_LAP2
             else:
@@ -559,9 +559,9 @@ class FastLapChain(object):
                 self.et.set_start_yaw(start_yaw)
             else:
                 if self.course == "right":
-                    return None, (100, 90, 0), Mode.SHORTCUT_LAP2
+                    return None, (100, 80, 0), Mode.SHORTCUT_LAP2
                 else:
-                    return None, (90, 100, 0), Mode.SHORTCUT_LAP2
+                    return None, (80, 100, 0), Mode.SHORTCUT_LAP2
 
         # フェーズ4: position_startとの差分2000未満ならyaw_straight_control直進。2000以上で次フェーズ、基準yaw更新（直進区間）
         if phase.get_phase() == 4:
@@ -569,7 +569,7 @@ class FastLapChain(object):
             current_pos = self.get_motor_position(self.course)
             position_diff = abs(current_pos - position_start)
             # if position_diff < 1750:
-            if position_diff < 1200:
+            if position_diff < 1300:
                 left_speed, right_speed = et.yaw_straight_control(base_speed=HIGH_SPEED_BASE)
                 return None, (left_speed, right_speed, 0), Mode.SHORTCUT_LAP2
             else:
