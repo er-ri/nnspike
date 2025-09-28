82944c8c4ac11cb3927adc043203144ca5e7f49e
Nakazawa <luckystriq@gmail.com>
2025-09-28 19:50:56 +0900

    1

--- nnspike/unit/etrobot.py
+++ nnspike/unit/etrobot.py
@@ -484,7 +484,8 @@ class ETRobot(object):
         vertical_poles = [0.0, -180.0, 180.0]
         nearest = min(vertical_poles, key=lambda p: abs(self.wrap_angle(yaw - p)))
         self._start_yaw = nearest
-
+        print(f"[DEBUG][set_start_yaw_nearest_vertical_pole] set={self._start_yaw}")
+        
     def set_start_yaw_nearest_horizontal_pole(self):
         """
         get_yawで取得した値を+90, -90, +270, -270（横方向の基準）にラップしてself._start_yawに設定する。
@@ -493,3 +494,4 @@ class ETRobot(object):
         horizontal_poles = [90.0, -90.0, 270.0, -270.0]
         nearest = min(horizontal_poles, key=lambda p: abs(self.wrap_angle(yaw - p)))
         self._start_yaw = nearest
+        print(f"[DEBUG][set_start_yaw_nearest_horizontal_pole] set={self._start_yaw}")
