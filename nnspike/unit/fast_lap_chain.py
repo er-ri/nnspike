93aee3bac61318f76f7cc722c9abedffbe1238a4
Tsubasa717 <Tsubasa717@users.noreply.github.com>
2025-10-10 18:47:48 +0900

    １

--- nnspike/unit/fast_lap_chain.py
+++ nnspike/unit/fast_lap_chain.py
@@ -201,6 +201,8 @@ class FastLapChain(object):
                 left_speed, right_speed = et.yaw_straight_control(base_speed=HIGH_SPEED_BASE)
                 return None, (left_speed, right_speed, 0), Mode.FAST_LAP
             else:
+                import sys
+                sys.exit("強制終了")
                 phase.next_phase()
                 phase.set_position_start("position_start", self.get_motor_position(self.course))
 
