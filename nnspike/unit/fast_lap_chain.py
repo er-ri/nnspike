9259c745c9d6853da3f63da004dc95ab82e6bd25
Tsubasa717 <Tsubasa717@users.noreply.github.com>
2025-10-10 19:09:13 +0900

    1

--- nnspike/unit/fast_lap_chain.py
+++ nnspike/unit/fast_lap_chain.py
@@ -212,14 +212,14 @@ class FastLapChain(object):
                 phase.next_phase()
                 phase.set_position_start("position_start", self.get_motor_position(self.course))
                 if self.course == "right":
-                    self.et.set_start_yaw(start_yaw + 30.0)  # 右コースは+30度
+                    self.et.set_start_yaw(start_yaw - 30.0)  # 右コースは+30度
                 else:
-                    self.et.set_start_yaw(start_yaw - 30.0)  # 左コースは-30度
+                    self.et.set_start_yaw(start_yaw + 30.0)  # 左コースは-30度
             else:
                 if self.course == "right":
-                    return None, (70, 100, 0), Mode.FAST_LAP
-                else:
                     return None, (100, 70, 0), Mode.FAST_LAP
+                else:
+                    return None, (70, 100, 0), Mode.FAST_LAP
 
         # フェーズ2: position_startとの差分500未満ならyaw_straight_control直進。500以上で次フェーズ、基準yaw更新（直進区間）
         if phase.get_phase() == 2:
