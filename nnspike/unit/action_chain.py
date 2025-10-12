bab56e0e0d0f6ec2d7b63f5f4860971659855ad6
Nakazawa <luckystriq@gmail.com>
2025-10-12 15:25:55 +0900

    １

--- nnspike/unit/action_chain.py
+++ nnspike/unit/action_chain.py
@@ -1359,7 +1359,7 @@ class ActionChain(object):
             else:
                 self._reset_acceleration_timer()
                 et.set_start_yaw()
-                print(f"[DEBUG] mode={Mode.TEST.value} | phase={phase.get_phase()} | position_diff={position_diff} >= 1000 | start_yaw={et.get_start_yaw():.2f} | current_yaw=PHASE_CHANGE | proceed to phase1")
+                print(f"[DEBUG] mode={Mode.TEST.value} | phase={phase.get_phase()} | position_diff={position_diff} >= 1000 | start_yaw={{et.get_start_yaw():.2f}} | current_yaw=PHASE_CHANGE | proceed to phase1")
                 phase.next_phase(current_pos)
                 return (0, 0), Mode.TEST
 
@@ -1377,7 +1377,7 @@ class ActionChain(object):
                 # else:
                 #     et.set_start_yaw(et.get_start_yaw() + 90.0)
                 et.set_start_yaw()
-                print(f"[DEBUG] mode={Mode.TEST.value} | phase={phase.get_phase()} | turn position_diff={position_diff} >= 390 | start_yaw={et.get_start_yaw():.2f} | current_yaw=PHASE_CHANGE | proceed to phase2")
+                print(f"[DEBUG] mode={Mode.TEST.value} | phase={phase.get_phase()} | turn position_diff={position_diff} >= 385 | start_yaw={{et.get_start_yaw():.2f}} | current_yaw=PHASE_CHANGE | proceed to phase2")
                 phase.next_phase(current_pos)
                 return (0, 0), Mode.TEST
 
@@ -1407,7 +1407,7 @@ class ActionChain(object):
             else:
                 self._reset_acceleration_timer()
                 et.set_start_yaw()
-                print(f"[DEBUG] mode={Mode.TEST.value} | phase={phase.get_phase()} | position_diff={position_diff} >= 1000 | start_yaw={et.get_start_yaw():.2f} | current_yaw=PHASE_CHANGE | proceed to phase3")
+                print(f"[DEBUG] mode={Mode.TEST.value} | phase={phase.get_phase()} | position_diff={position_diff} >= 1000 | start_yaw={{et.get_start_yaw():.2f}} | current_yaw=PHASE_CHANGE | proceed to phase3")
                 phase.next_phase(current_pos)
                 return (0, 0), Mode.TEST
 
@@ -1425,7 +1425,7 @@ class ActionChain(object):
                 # else:
                 #     et.set_start_yaw(et.get_start_yaw() + 90.0)
                 et.set_start_yaw()
-                print(f"[DEBUG] mode={Mode.TEST.value} | phase={phase.get_phase()} | turn position_diff={position_diff} >= 390 | start_yaw={et.get_start_yaw():.2f} | current_yaw=LOOP | proceed to phase0")
+                print(f"[DEBUG] mode={Mode.TEST.value} | phase={phase.get_phase()} | turn position_diff={position_diff} >= 385 | start_yaw={{et.get_start_yaw():.2f}} | current_yaw=LOOP | proceed to phase0")
                 phase.next_phase(current_pos, skip=-3)  # 0に戻す
                 return (0, 0), Mode.TEST
 
