09099951f8f10ce2b4352fdbbda852d8b486cb5b
Nakazawa <luckystriq@gmail.com>
2025-10-12 13:46:25 +0900

    １

--- run_realtime.py
+++ run_realtime.py
@@ -323,10 +323,11 @@ def main(record_sensor_data=False, save_camera_video=False, course="right", cour
                     et.set_motor_forward_speed(left_speed=left_speed, right_speed=right_speed)
                 case Mode.EYE_BLUE:
                     (left_speed, right_speed), mode = unpack_action_result(action_chain.eye_blue(frame))
-                    et.set_motor_forward_speed(left_speed=left_speed, right_speed=right_speed)
+                    et.set_motor_speed(left_speed=left_speed, right_speed=right_speed)
                 case Mode.TEST:
                     (left_speed, right_speed), mode = unpack_action_result(action_chain.test_mode_action(frame))
-                    et.set_motor_forward_speed(left_speed=left_speed, right_speed=right_speed)
+                    et.set_motor_speed(left_speed=left_speed, right_speed=right_speed)
+                    
                 case Mode.PAUSE:
                     left_speed, right_speed = 0, 0
                     et.set_motor_forward_speed(left_speed=left_speed, right_speed=right_speed)
