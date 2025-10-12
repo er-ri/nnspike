a266b1fbf1025fbf9078694e0ede89b60d0b52e7
Nakazawa <luckystriq@gmail.com>
2025-10-12 15:25:24 +0900

    １

--- nnspike/unit/action_chain.py
+++ nnspike/unit/action_chain.py
@@ -1366,7 +1366,7 @@ class ActionChain(object):
         # phase1: 90度旋回（course依存で左右、距離390進むまで、速度30）
         if phase.get_phase() == 1:
             position_diff = phase.get_position_diff(current_pos)
-            if position_diff < 390:
+            if position_diff < 385:
                 if self.course == "right":
                     return (0, 20), Mode.TEST
                 else:
@@ -1414,7 +1414,7 @@ class ActionChain(object):
         # phase3: 90度旋回（course依存で左右、距離390進むまで、速度30）
         if phase.get_phase() == 3:
             position_diff = phase.get_position_diff(current_pos)
-            if position_diff < 390:
+            if position_diff < 385:
                 if self.course == "right":
                     return (0, 20), Mode.TEST
                 else:
