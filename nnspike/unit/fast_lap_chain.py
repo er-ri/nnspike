b502711987fe09190875cfc57d0eaadc8bdbb29d
Tsubasa717 <Tsubasa717@users.noreply.github.com>
2025-10-20 20:05:52 +0900

    1

--- nnspike/unit/fast_lap_chain.py
+++ nnspike/unit/fast_lap_chain.py
@@ -199,7 +199,7 @@ class FastLapChain(object):
             position_start = phase.get_position_start("position_start")
             current_pos = self.get_motor_position(self.course)
             position_diff = abs(current_pos - position_start)
-            if position_diff < 5000:
+            if position_diff < 6000:
                 left_speed, right_speed = et.yaw_straight_control(base_speed=HIGH_SPEED_BASE, deadband= 2.0, adjust_speed= 10)
                 return None, (left_speed, right_speed, 0), Mode.FAST_LAP
             else:
