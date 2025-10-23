2390119cd32df6b93932f316204faadfe170489d
Tsubasa717 <Tsubasa717@users.noreply.github.com>
2025-10-23 18:11:47 +0900

    1

--- nnspike/unit/fast_lap_chain.py
+++ nnspike/unit/fast_lap_chain.py
@@ -236,7 +236,7 @@ class FastLapChain(object):
 
         # フェーズ3: 左旋回60度判定（ここでは20度基準）。到達で次フェーズ、基準yaw更新（左旋回区間）
         if phase.get_phase() == 3:
-            stop_turn = et.is_yaw_turn_finished(side=self.course, threshold_deg=90.0)
+            stop_turn = et.is_yaw_turn_finished(side=self.course, threshold_deg=85.0)
             if stop_turn:
                 sys.exit("")
                 phase.next_phase()
