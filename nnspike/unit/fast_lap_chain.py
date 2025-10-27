2f832e73295b290a9c974c51b5e3cc6f5e5ad2d7
Tsubasa717 <Tsubasa717@users.noreply.github.com>
2025-10-27 18:46:55 +0900

    1

--- nnspike/unit/fast_lap_chain.py
+++ nnspike/unit/fast_lap_chain.py
@@ -289,7 +289,7 @@ class FastLapChain(object):
             position_start = phase.get_position_start("position_start")
             current_pos = self.get_motor_position(self.course)
             position_diff = abs(current_pos - position_start)
-            if position_diff < 4550:
+            if position_diff < 4700:
                 left_speed, right_speed = et.yaw_straight_control(base_speed=HIGH_SPEED_BASE)
                 return None, (left_speed, right_speed, 0), Mode.FAST_LAP
             else:
@@ -331,9 +331,9 @@ class FastLapChain(object):
                 phase.next_phase()
                 phase.set_position_start("position_start", self.get_motor_position(self.course))
                 if self.course == "right":
-                    self.et.set_start_yaw(start_yaw - 85.0)
+                    self.et.set_start_yaw(start_yaw - 60.0)
                 else:
-                    self.et.set_start_yaw(start_yaw + 85.0)
+                    self.et.set_start_yaw(start_yaw + 60.0)
             else:
                 if self.course == "right":
                     return None, (80, 100, 0), Mode.FAST_LAP
@@ -349,7 +349,6 @@ class FastLapChain(object):
                 left_speed, right_speed = et.yaw_straight_control(base_speed=HIGH_SPEED_BASE)
                 return None, (left_speed, right_speed, 0), Mode.FAST_LAP
             else:
-                sys.exit("")
                 phase.next_phase()
                 phase.set_position_start("position_start", self.get_motor_position(self.course))
 
