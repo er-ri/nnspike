8f709976b84aea2b5a67333c70426a63991964e3
Tsubasa717 <Tsubasa717@users.noreply.github.com>
2025-10-27 14:36:42 +0900

    1

--- nnspike/unit/fast_lap_chain.py
+++ nnspike/unit/fast_lap_chain.py
@@ -325,6 +325,7 @@ class FastLapChain(object):
         if phase.get_phase() == 3:
             stop_turn = et.is_yaw_turn_finished(side=self.opposite_course, threshold_deg=90.0)
             if stop_turn:
+                sys.exit("")
                 phase.next_phase()
                 phase.set_position_start("position_start", self.get_motor_position(self.course))
                 if self.course == "right":
