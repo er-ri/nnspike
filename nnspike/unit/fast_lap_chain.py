583741df087a973658368f8b481dd84c845b0cce
Tsubasa717 <Tsubasa717@users.noreply.github.com>
2025-10-27 14:51:50 +0900

    1

--- nnspike/unit/fast_lap_chain.py
+++ nnspike/unit/fast_lap_chain.py
@@ -314,7 +314,7 @@ class FastLapChain(object):
             position_start = phase.get_position_start("position_start")
             current_pos = self.get_motor_position(self.course)
             position_diff = abs(current_pos - position_start)
-            if position_diff < 350:
+            if position_diff < 30:
                 left_speed, right_speed = et.yaw_straight_control(base_speed=HIGH_SPEED_BASE)
                 return None, (left_speed, right_speed, 0), Mode.FAST_LAP
             else:
@@ -323,15 +323,15 @@ class FastLapChain(object):
 
         # フェーズ3: 左旋回60度（is_yaw_turn_finished判定）。到達で次フェーズ、基準yaw更新（左旋回区間）
         if phase.get_phase() == 3:
-            stop_turn = et.is_yaw_turn_finished(side=self.opposite_course, threshold_deg=65.0)
+            stop_turn = et.is_yaw_turn_finished(side=self.opposite_course, threshold_deg=75.0)
             if stop_turn:
                 sys.exit("")
                 phase.next_phase()
                 phase.set_position_start("position_start", self.get_motor_position(self.course))
                 if self.course == "right":
-                    self.et.set_start_yaw(start_yaw - 65.0)  # 右コースは-30度
+                    self.et.set_start_yaw(start_yaw - 75.0)  # 右コースは-30度
                 else:
-                    self.et.set_start_yaw(start_yaw + 65.0)  # 左コースは+30度
+                    self.et.set_start_yaw(start_yaw + 75.0)  # 左コースは+30度
             else:
                 if self.course == "right":
                     return None, (90, 100, 0), Mode.FAST_LAP
