6cd99cf82f6298984a70bd2e90c12fbd070c8841
Nakazawa <luckystriq@gmail.com>
2025-10-28 15:32:09 +0900

    １

--- nnspike/unit/action_chain.py
+++ nnspike/unit/action_chain.py
@@ -643,7 +643,7 @@ class ActionChain(object):
         if phase.get_phase() == 0:
             position_diff = phase.get_position_diff(current_pos)
             if position_diff < 600:
-                return (-BASE_SPEED, -BASE_SPEED), Mode.BACK_AND_TURN1
+                return (-30, -30), Mode.BACK_AND_TURN1
             print(f"[DEBUG] mode={Mode.BACK_AND_TURN1.value} | phase={phase.get_phase()} | position_diff={position_diff} >= 600")
             phase.next_phase(current_pos)
 
