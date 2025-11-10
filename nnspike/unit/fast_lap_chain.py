593edb0c2f6f2a10e4a96d3362b9e2a63df6e899
Nakazawa <luckystriq@gmail.com>
2025-11-10 16:55:48 +0900

    1

--- nnspike/unit/fast_lap_chain.py
+++ nnspike/unit/fast_lap_chain.py
@@ -397,7 +397,7 @@ class FastLapChain(object):
         # フェーズ0: course側モータ距離3000未満ならyaw_straight_controlで直進（右コースなら+5度、左コースなら-5度に自然調整）。3000以上で次フェーズ
         if phase.get_phase() == 0:
             position_diff = phase.get_position_diff(current_pos)
-            if position_diff < 3500:
+            if position_diff < 3800:
                 # ファストラップ専用スタートダッシュ加速制御メソッドを使用
                 base_speed = self.get_fast_lap_start_dash_speed(HIGH_SPEED_BASE, 0.5)
                 left_speed, right_speed = et.yaw_straight_control(base_speed=base_speed, deadband=2.0, adjust_speed=2)
