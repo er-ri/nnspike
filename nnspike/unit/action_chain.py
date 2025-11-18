4b4e133c5148fb8d7d3ac68541b62dc852183e39
Nakazawa <luckystriq@gmail.com>
2025-11-18 17:32:23 +0900

    1

--- nnspike/unit/action_chain.py
+++ nnspike/unit/action_chain.py
@@ -592,7 +592,6 @@ class ActionChain(object):
             distance_from_start = phase.get_position_diff(current_pos)
             # 計算距離到達で停止
             if distance_from_start >= self._calculated_distance + 10:
-
                 print(f"[DEBUG] mode={Mode.CARRY_BOTTLE1.value} | phase={phase.get_phase()} | distance_from_start={distance_from_start} >= _calculated_distance={self._calculated_distance} | STOP | start_yaw={et.get_start_yaw():.2f} | current_yaw={et.get_yaw():.2f}")
                 phase.next_phase(current_pos)
                 return (0, 0), Mode.CARRY_BOTTLE1
