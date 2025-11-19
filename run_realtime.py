84037d97f62e034bb95a0bd752dc933b58184ab4
Nakazawa <luckystriq@gmail.com>
2025-11-19 16:28:53 +0900

    1

--- run_realtime.py
+++ run_realtime.py
@@ -234,7 +234,8 @@ def main(record_sensor_data=False, save_camera_video=False, course="right", cour
 
     # wait_for_start()の後にmodeの初期値を決定
     if state_flags.force_sensor_mode_switch_enabled:
-        mode = Mode.FAST_LAP
+        #mode = Mode.FAST_LAP
+        mode = Mode.FAST_LAP2
     else:
         mode = Mode.PAUSE
 
