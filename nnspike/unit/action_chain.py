615ec4fa8781b66096e83ea951007144c0d85320
Nakazawa <luckystriq@gmail.com>
2025-11-19 16:45:59 +0900

    １

--- nnspike/unit/action_chain.py
+++ nnspike/unit/action_chain.py
@@ -866,7 +866,7 @@ class ActionChain(object):
         if phase.get_phase() == 8:
             position_diff = phase.get_position_diff(current_pos)
             if position_diff < 400:
-                if self.course_type == "upper":
+                if self.course_type != "upper":
                     return (BASE_SPEED, BASE_SPEED), Mode.CARRY_BOTTLE2
                 elif self.course == "right":
                     return (BASE_SPEED + 5, BASE_SPEED), Mode.CARRY_BOTTLE2
